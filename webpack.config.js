56a4d7fcbd20836d5d22153e6975b7012e8330a9
Timson020 <a_sa_f@126.com>
2018-07-13 19:12:43 +0800

    优化调整

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,62 @@
 var path = require('path')
+var isProd = true
 
-module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, './dist'),
-		publicPath: '/dist/',
-		filename: `build.js`
-	},
-	module: {
-		rules: [{
-			test: /\.jsx$/,
-			loader: 'babel-loader',
-			exclude: /node_modules/
-		}, {
-			test: /\.js$/,
-			loader: 'babel-loader',
-			exclude: /node_modules/
-		}, {
-			test: /\.css$/,
-			loader: 'style-loader!css-loader'
-		}, {
-			test: /\.(scss||sass)$/,
-			loader: 'style-loader!css-loader!sass-loader'
-		}, {
-			test: /\.(eot|ttf|woff|woff2)$/,
-			loader: 'file-loader'
-		}, {
-			test: /\.(png|jpg|gif|svg)$/,
-			loader: 'file-loader',
-			options: {
-				name: '[name].[ext]?[hash]'
-			}
-		}]
-	},
-	resolve: {
-		extensions: ['.jsx', '.js', '.json', '.scss', '.css'],
-		alias: {
-			'@': path.resolve(__dirname, './src')
-		}
-	},
-	devServer: {
-		historyApiFallback: true,
-		noInfo: true
-	},
-	target: 'web',
-	devtool: '#eval-source-map'
+// 默认配置
+const defaultconfig = {
+  target: 'web',
 }
 
+// 开发环境配置
+const developmentconfig = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: `build.js`
+  },
+  module: {
+    rules: [{
+      test: /\.jsx$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/
+    }, {
+      test: /\.css$/,
+      loader: 'style-loader!css-loader'
+    }, {
+      test: /\.(scss||sass)$/,
+      loader: 'style-loader!css-loader!sass-loader'
+    }, {
+      test: /\.(eot|ttf|woff|woff2)$/,
+      loader: 'file-loader'
+    }, {
+      test: /\.(png|jpg|gif|svg)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]?[hash]'
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.jsx', '.js', '.json', '.scss', '.css'],
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: '#eval-source-map'
+}
+
+
+// 生产环境打包配置
+const releaseconfig = {}
+
+const config = Object.assign(defaultconfig, isProd ? releaseconfig : developmentconfig)
+
+module.exports = config
